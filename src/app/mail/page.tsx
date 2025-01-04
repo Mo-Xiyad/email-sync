ff49d1b0066fe3f40f887e530255000ea3265fba
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2025-01-04 18:28:38 +0100

    render dynamicly

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import dynamic from "next/dynamic";
+
+const Mail = dynamic(
+  () => import("./components/mail").then((mod) => mod.Mail),
+  {
+    ssr: false,
+  },
+);
 function page() {
   return (
     <div className="hidden h-screen flex-col overflow-scroll md:flex">
-      <h1>dashboard</h1>
+      <Mail
+        defaultLayout={[20, 32, 48]}
+        defaultCollapsed={false}
+        navCollapsedSize={4}
+      />
     </div>
   );
 }
